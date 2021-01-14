e1c0feb3b6886f300973b69e8e05255e97060da3
EvanDbg <yangf1023@icloud.com>
2021-01-14 13:55:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,28 @@
 console.log('ok test');
+
+const $ = new Env();
+const options = {
+  url: 'https://api.day.app/oscvrdJCinKuuuf2dSUXhL/okok'
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+}
+$.get(options, (err, resp, data) => {
+  try {
+    if (err) {
+      console.log('Bark APP发送通知调用API失败！！\n')
+      console.log(err);
+    } else {
+      data = JSON.parse(data);
+      if (data.code === 200) {
+        console.log('Bark APP发送通知消息成功\n')
+      } else {
+        console.log(`${data.message}\n`);
+      }
+    }
+  } catch (e) {
+    $.logErr(e, resp);
+  } finally {
+
+  }
+})
